909afcf59bb572fa6cce1d0e2867c2aa59c8fa24
Mause Javier <mause.javi@gmail.com>
2025-02-15 18:29:20 -0300

    modificando precios publico

--- resources/views/livewire/precios-publico/precios-publico.blade.php
+++ resources/views/livewire/precios-publico/precios-publico.blade.php
@@ -43,12 +43,12 @@
                 </label>
             </article>
             <div x-data="fullscreenHandler()" class="p-4 border rounded bg-gray-100">
-                <p class="mb-4">Presiona el botón para alternar el modo de pantalla completa.</p>
+                <p class="mb-4">Presiona el botón para pantalla completa.</p>
                 <button 
                     x-on:click="toggleFullscreen" 
                     class="px-4 py-2 bg-blue-500 text-white rounded"
                 >
-                    Alternar Pantalla Completa
+                    Pantalla Completa
                 </button>
             </div>
         </div>
@@ -73,14 +73,14 @@
                             this.isFullscreen = true;
                         } else {
                             // Salir de pantalla completa
-                            if (document.exitFullscreen) {
-                                await document.exitFullscreen();
-                            } else if (document.webkitExitFullscreen) { // Safari
-                                await document.webkitExitFullscreen();
-                            } else if (document.msExitFullscreen) { // IE11
-                                await document.msExitFullscreen();
-                            }
-                            this.isFullscreen = false;
+                            //if (document.exitFullscreen) {
+                            //    await document.exitFullscreen();
+                            //} else if (document.webkitExitFullscreen) { // Safari
+                            //    await document.webkitExitFullscreen();
+                            //} else if (document.msExitFullscreen) { // IE11
+                            //    await document.msExitFullscreen();
+                            //}
+                            //this.isFullscreen = false;
                         }
                     }
                 };
